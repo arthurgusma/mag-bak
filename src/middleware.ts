ee3e348510c62bdd144f2d63454154ff2f264840
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-12-04 11:29:20 -0300

    fix: unnecessary use og getToken form middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,4 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { getToken } from 'next-auth/jwt'
 import { cookies } from 'next/headers'
 
 const privateRoutes = ['/home']
@@ -8,17 +7,11 @@ export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname
   const isPrivate = privateRoutes.includes(path)
 
-  // Get the JWT token from the request
-  const token = await getToken({ req })
   const sessionCookie = (await cookies()).get('next-auth.session-token')?.value
 
-  console.log('Token:', token)
-  console.log('Session cookie:', sessionCookie)
-
-  // Redirect or block access if on a private route and not authenticated
   if (isPrivate && !sessionCookie) {
-    return NextResponse.redirect(new URL('/', req.url)) // Redirect to login
+    return NextResponse.redirect(new URL('/', req.url))
   }
 
-  return NextResponse.next() // Allow the request to proceed
+  return NextResponse.next()
 }

03aaf24c5e85160a3632adfbf7db40968d33eab2
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-12-04 11:51:14 -0300

    refacotr: removes stile from root layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import HomeForm from '@/components/HomeForm'
 
 export default function Home() {
   return (
-    <main className="w-1/4 min-h-svh place-content-center">
-      <h1 className="text-xl">Bem vindo ao Mag Bak</h1>
-      <HomeForm />
+    <main className="flex justify-center ">
+      <div className="w-1/4 min-h-svh place-content-center">
+        <h1 className="text-xl">Bem vindo ao Mag Bak</h1>
+        <HomeForm />
+      </div>
     </main>
   )
 }

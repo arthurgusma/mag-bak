e4817e4f77f1c5a412d60a4fe260d8fa82599f20
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-12-05 17:23:14 -0300

    feat: helpers funcitons to format date and currency

---
+++ src/helpers/index.ts
@@ -0,0 +1,19 @@
+export const { format: formartCurrencyToReal } = new Intl.NumberFormat(
+  'pt-BR',
+  {
+    style: 'currency',
+    currency: 'BRL',
+  },
+)
+
+export function formatIsoDate(isoDate: string) {
+  const date = new Date(isoDate)
+
+  const day = String(date.getDate()).padStart(2, '0')
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const year = date.getFullYear()
+  const hours = String(date.getHours()).padStart(2, '0')
+  const minutes = String(date.getMinutes()).padStart(2, '0')
+
+  return `${day}/${month}/${year} às ${hours}:${minutes}`
+}

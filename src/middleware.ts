db2f8c7569d68ad1778df2c2d46e027351f943c8
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-12-06 14:45:44 -0300

    fix: updates middleware to fix deploy on vercel

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,11 @@ export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname
   const isPrivate = privateRoutes.includes(path)
 
-  const sessionCookie = (await cookies()).get('next-auth.session-token')?.value
+  const sessionCookie = (await cookies()).get(
+    process.env.NEXT_PUBLIC_AMBIENT! === 'prod'
+      ? '__Secure-next-auth.session-token'
+      : 'next-auth.session-token',
+  )?.value
 
   if (isPrivate && !sessionCookie) {
     return NextResponse.redirect(new URL('/', req.url))

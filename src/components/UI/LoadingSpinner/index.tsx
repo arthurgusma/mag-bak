65e3c07410e2444d19338447dd48978df93d4c7e
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-12-06 08:54:20 -0300

    feat: a loading spinner component

---
+++ src/components/UI/LoadingSpinner/index.tsx
@@ -0,0 +1,7 @@
+export default function LoadingSpinner() {
+  return (
+    <section className="flex items-center justify-center h-60">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-green-500 border-opacity-75"></div>
+    </section>
+  )
+}
